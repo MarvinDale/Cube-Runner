caf5a6b7e1a62040a9ee549cad0b3960e4c544dd
MarvinDale <marvinfrancisdale@gmail.com>
2020-03-10 21:35:51 +0000

    Add project

---
+++ Cube Runner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 500f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d")) // If the player is pressing the "d" key
+        {
+            //Add a force to the right
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))//if the player is pressing the "a" key
+        {
+            //Add a force to the left
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
